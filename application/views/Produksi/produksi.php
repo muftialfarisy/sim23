d87698cddea6923969338baa7a9e65639e75aa3c
muftialfarisy <alfabjp@gmail.com>
2023-01-01 16:19:27 +0700

    update v1.1

--- application/views/Produksi/produksi.php
+++ application/views/Produksi/produksi.php
@@ -22,7 +22,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Produksigit</h1>
+                            <h1>Produksi</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
